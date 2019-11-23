680f0c481cd0f1fb278e7d1522860c2a758f5523
Wagner Leonardi <leonardiwagner@gmail.com>
2019-11-22 23:36:43 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,8 @@
 # install sshpass to by pass password manual ask
 sudo apt-get update
 sudo apt-get install sshpass
-pwd
-ls
 # compress files to deploy
-tar -czf deploy.tar.gz *
-ls
-pwd
+tar -cvzf deploy.tar.gz *
 # upload compressed files to the server
 sshpass -p $INSTANCE_PASSWORD scp -o StrictHostKeyChecking=no -rp deploy.tar.gz cd@$INSTANCE_IP:/opt/application
 # stop running application
@@ -17,7 +13,6 @@ sshpass -p $INSTANCE_PASSWORD ssh cd@$INSTANCE_IP << EOF
   mv /opt/application/current /opt/application/$(date +"%Y-%m-%d_%H-%M-%S")
   killall node
   tar -zxf /opt/application/deploy.tar.gz /opt/application/current
-  rm /opt/application/deploy.tar.gz
   mkdir /opt/application/current
   cd /opt/application/current
   npm install
